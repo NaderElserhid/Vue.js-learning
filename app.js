633a5a78335f51d74fb773e21ed91a247dcf9fac
nadir <nadirelserhid@gmail.com>
2024-09-10 12:25:16 +0300

    favoret item feture is ready

--- app.js
+++ app.js
@@ -2,14 +2,32 @@ const app = Vue.createApp({
   data(){
     return {
         showBook: true,
-        title : 'clean code',
-        lastName  : 'Elserhid',
-        age : 26,
+        x:0,
+        y:0,
+     books:[
+        {title : 'clean code', author  : 'nader' , date : 2020 ,img: 'assets/1.jpg' , isFav : true},
+        {title : 'moon ', author  : 'elserhid', date : 2022, img: 'assets/2.jpg', isFav : false} ,
+        {title : 'one way ', author  : 'maher' , date : 2000 , img: 'assets/3.jpg', isFav : true},
+     ]
   }
 },
 methods: {
     toggleShowBooks(){
        this.showBook = !this.showBook
+    },
+    handelEvent(e){
+        console.log(e ,e.type);
+        if(data){
+            console.log(data);
+            
+        }
+    },
+    handlemove(e){
+     this.x = e.offsetX
+     this.y = e.offsetY
+    },
+    showFav(data){
+      data.isFav =!data.isFav 
     }
   }
 })
